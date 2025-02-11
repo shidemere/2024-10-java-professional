4e8ed0da713143de6e8ce8654fc89fbb17d54819
prometheus <tihopadaetsneg@gmail.com>
2025-02-11 17:37:49 +0300

    init gradle parent project

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+//Описание того, как собрать проект
+
+import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
+
+plugins {
+    idea
+}
+
+idea {
+    project {
+        languageLevel = IdeaLanguageLevel(21)
+    }
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
+}
+
+
+allprojects {
+    group = "ru.otus"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+
+}
+
+subprojects {
+    plugins.apply(JavaPlugin::class.java)
+    extensions.configure<JavaPluginExtension> {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+// Настройки проекта
+
+rootProject.name = "2024-10-java-professional"
+
+pluginManagement {
+    val dependencyManagement: String by settings
+    val springframeworkBoot: String by settings
+
+    plugins {
+        id("io.spring.dependency-management") version dependencyManagement
+        id("org.springframework.boot") version springframeworkBoot
+    }
+}
