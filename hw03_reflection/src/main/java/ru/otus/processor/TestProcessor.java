9ed435d8e2490b3edd8c1d18708c0032d4ac60fc
shidemere <tihopadaetsneg@gmail.com>
2025-02-20 14:22:36 +0300

    [Homework 03] fix by comments

--- hw03_reflection/src/main/java/ru/otus/processor/TestProcessor.java
+++ hw03_reflection/src/main/java/ru/otus/processor/TestProcessor.java
@@ -1,17 +1,26 @@
 package ru.otus.processor;
 
 import java.lang.reflect.Method;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.otus.annotation.After;
 import ru.otus.annotation.Before;
 import ru.otus.annotation.Test;
 
 public class TestProcessor<T> {
-    private final T t;
-    private int successCounter;
-    private int failCounter;
-    private int allTestCounter;
+    private static final Logger logger = LoggerFactory.getLogger(TestProcessor.class);
 
     public TestProcessor(Class<T> clazz) {
+
+        /*
+           Из за запрета на состояние пришлось использовать массив.
+           Так как обертки, оказывается, неизменяемы.
+           И если я передаю внутрь другого метода обертку - внутри метода создаются новые.
+        */
+        int[] successCounter = new int[1];
+        int[] failCounter = new int[1];
+        int[] allTestCounter = new int[1];
+        T t;
 
         try {
             t = clazz.getDeclaredConstructor().newInstance();
@@ -22,51 +31,32 @@ public class TestProcessor<T> {
         Method[] declaredMethods = clazz.getDeclaredMethods();
         for (Method method : declaredMethods) {
             if (method.isAnnotationPresent(Before.class)) {
-                processBeforeAnnotation(method);
+                invokeWithCounter(t, method, allTestCounter, successCounter, failCounter);
             } else if (method.isAnnotationPresent(Test.class)) {
-                processTestAnnotation(method);
+                invokeWithCounter(t, method, allTestCounter, successCounter, failCounter);
             } else if (method.isAnnotationPresent(After.class)) {
-                processAfterAnnotation(method);
+                invokeWithCounter(t, method, allTestCounter, successCounter, failCounter);
             }
         }
 
-        if (successCounter == 0 && failCounter == 0) {
-            System.out.println("Class is not annotated with annotations for testing.");
+        if (successCounter[0] == 0 && failCounter[0] == 0) {
+            logger.warn("Class is not annotated with annotations for testing.");
         }
 
-        System.out.println("Testing is done");
-        System.out.printf("All tests: %s\n", allTestCounter);
-        System.out.printf("Success tests: %s\n", successCounter);
-        System.out.printf("Failed tests: %s\n", failCounter);
+        logger.info("Testing is done");
+        logger.info("All tests: {}", allTestCounter[0]);
+        logger.info("Success tests: {}", successCounter[0]);
+        logger.info("Failed tests: {}", failCounter[0]);
     }
 
-    private void processBeforeAnnotation(Method method) {
-        allTestCounter++;
+    private void invokeWithCounter(
+            Object target, Method method, int[] allTestCounter, int[] successCounter, int[] failCounter) {
+        allTestCounter[0]++;
         try {
-            method.invoke(t);
-            successCounter++;
+            method.invoke(target);
+            successCounter[0]++;
         } catch (Exception e) {
-            failCounter++;
-        }
-    }
-
-    private void processTestAnnotation(Method method) {
-        allTestCounter++;
-        try {
-            method.invoke(t);
-            successCounter++;
-        } catch (Exception e) {
-            failCounter++;
-        }
-    }
-
-    private void processAfterAnnotation(Method method) {
-        allTestCounter++;
-        try {
-            method.invoke(t);
-            successCounter++;
-        } catch (Exception e) {
-            failCounter++;
+            failCounter[0]++;
         }
     }
 }
